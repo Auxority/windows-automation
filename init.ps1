72aff3c990c4019ed64dfb239d0cb52d0168d15e
Auxority <gide0013@hz.nl>
2023-01-25 13:59:30 +0100

    simplified installation of winget applications

--- init.ps1
+++ init.ps1
@@ -1,60 +1,64 @@
 # Save the current directory
 $pwd = Get-Location
 
-# Install Geforce Experience
-winget install --id Nvidia.GeForceExperience -e --source winget
+$WingetPrograms = @(
+    'Nvidia.GeForceExperience',
+    'Microsoft.PowerToys',
+    'Microsoft.VisualStudioCode',
+    'Git.Git',
+    'Brave.Brave',
+    'Discord.Discord',
+    'OpenJS.NodeJS',
+    'Valve.Steam',
+    'TIDALMusicAS.TIDAL',
+    'Oracle.JDK.19',
+    'Mojang.MinecraftLauncher',
+    'WhatsApp.WhatsApp',
+    'Corsair.iCUE.4'
+)
 
-# Install Visual Studio Code
-winget install --id Microsoft.VisualStudioCode -e --source winget
-
-# Install Git
-winget install --id Git.Git -e --source winget
-
-# Install Brave Browser
-winget install --id Brave.Brave -e --source winget
-
-# Install Discord
-winget install --id Discord.Discord -e --source winget
-
-# Install NodeJS
-winget install --id OpenJS.NodeJS -e --source winget
+# Install all the programs
+foreach ($program in $WingetPrograms) {
+    try {
+        winget install --id $program -e --source winget
+    } catch {
+        Write-Host "Failed to install $($program): $($_.Exception.Message)"
+    }
+}
 
 # Download and install Wargaming Game Center
-Invoke-WebRequest -Uri https://redirect.wargaming.net/WGC/Wargaming_Game_Center_Install_WoT_EU.exe -OutFile wargaming-installer.exe
-Start-Process .\wargaming-installer.exe -Wait
-Remove-Item .\wargaming-installer.exe
-
-# Install Steam
-winget install --id Valve.Steam -e --source winget
+try {
+    Invoke-WebRequest -Uri https://redirect.wargaming.net/WGC/Wargaming_Game_Center_Install_WoT_EU.exe -OutFile wargaming-installer.exe
+    Start-Process .\wargaming-installer.exe -Wait
+    Remove-Item .\wargaming-installer.exe
+} catch {
+    Write-Host "Failed to install Wargaming Game Center: $($_.Exception.Message)"
+}
 
 # Install FanControl
-cd ~/Downloads
-git clone https://github.com/Rem0o/FanControl.Releases.git FanControl
-cd FanControl
-Start-Process .\Updater.exe -Wait
-cd $pwd
-
-# Install Tidal
-winget install --id TIDALMusicAS.TIDAL -e --source winget
+try {
+    cd ~/Downloads
+    git clone https://github.com/Rem0o/FanControl.Releases.git FanControl
+    cd FanControl
+    Start-Process .\Updater.exe -Wait
+    cd $pwd
+} catch {
+    Write-Host "Failed to install FanControl: $($_.Exception.Message)"
+}
 
 # Install Paint.NET
-cd ~/Downloads
-$html = Invoke-RestMethod -Uri https://www.dotpdn.com/downloads/pdn.html
-$html -match 'download\/v(.*?)\/paint' | Out-Null
-$pdnVersion = $Matches[1]
-$downloadUrl = "https://github.com/paintdotnet/release/releases/download/v$pdnVersion/paint.net.$pdnVersion.winmsi.x64.zip"
-Invoke-WebRequest -Uri $downloadUrl -OutFile paintdotnet.zip
-Expand-Archive .\paintdotnet.zip -DestinationPath .\paintdotnet
-Start-Process .\paintdotnet\paint.net.$pdnVersion.winmsi.x64.msi -Wait
-Remove-Item .\paintdotnet.zip
-Remove-Item .\paintdotnet -Recurse
-cd $pwd
-
-# Install Java Development Kit
-winget install --id Oracle.JDK.19 -e --source winget
-
-# Install Minecraft
-winget install --id Mojang.MinecraftLauncher -e --source winget
-
-# Install WhatsApp Desktop
-winget install --id WhatsApp.WhatsApp -e --source winget
+try {
+    cd ~/Downloads
+    $html = Invoke-RestMethod -Uri https://www.dotpdn.com/downloads/pdn.html
+    $html -match 'download\/v(.*?)\/paint' | Out-Null
+    $pdnVersion = $Matches[1]
+    $downloadUrl = "https://github.com/paintdotnet/release/releases/download/v$pdnVersion/paint.net.$pdnVersion.winmsi.x64.zip"
+    Invoke-WebRequest -Uri $downloadUrl -OutFile paintdotnet.zip
+    Expand-Archive .\paintdotnet.zip -DestinationPath .\paintdotnet
+    Start-Process .\paintdotnet\paint.net.$pdnVersion.winmsi.x64.msi -Wait
+    Remove-Item .\paintdotnet.zip
+    Remove-Item .\paintdotnet -Recurse
+    cd $pwd
+} catch {
+    Write-Host "Failed to install Paint.NET: $($_.Exception.Message)"
+}
